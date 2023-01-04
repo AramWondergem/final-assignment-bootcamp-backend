433b62a332c39e56711eef46a0a2be033e4dad3b
AramWondergem <aramwondergem@gmail.com>
2023-01-04 12:43:52 +0100

    first commit with new spring project

---
+++ src/main/java/nl/wondergem/wondercooks/WondercooksApplication.java
@@ -0,0 +1,13 @@
+package nl.wondergem.wondercooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WondercooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WondercooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/wondergem/wondercooks/WondercooksApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.wondergem.wondercooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WondercooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
